831150320c8086ceb5e8f2f227acde593d585a9d
shrutiag14 <48885655+shrutiag14@users.noreply.github.com>
2021-06-05 19:12:52 +0530

    Update about.js

--- src/Components/pages/about.js
+++ src/Components/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>About Contest Arena</h1>
+            <h1>About Codechef-Peacer</h1>
             <p className='my-1'>
                 App
           </p>
